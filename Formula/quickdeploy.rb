8f6015f35b7cd29dbd15232f9cae08acdf3a73d7
Maximilian Kürschner <mail@programonaut.com>
2025-09-15 16:34:34 +0200

    initial formula

---
+++ Formula/quickdeploy.rb
@@ -0,0 +1,34 @@
+  class Quickdeploy < Formula
+    desc "One command deployment tool for your web applications"
+    homepage "https://quickdeploy.dev"
+    version "0.1.0"
+    license "MIT"
+
+    on_macos do
+      if Hardware::CPU.arm?
+        url "https://quickdeploy.dev/api/download/darwin-arm64"
+        sha256 "initial_sha_here" # darwin-arm64
+      else
+        url "https://quickdeploy.dev/api/download/darwin-amd64"
+        sha256 "initial_sha_here" # darwin-amd64
+      end
+    end
+
+    on_linux do
+      if Hardware::CPU.arm?
+        url "https://quickdeploy.dev/api/download/linux-arm64"
+        sha256 "initial_sha_here" # linux-arm64
+      else
+        url "https://quickdeploy.dev/api/download/linux-amd64"
+        sha256 "initial_sha_here" # linux-amd64
+      end
+    end
+
+    def install
+      bin.install Dir["*"].first => "quickdeploy"
+    end
+
+    test do
+      assert_match "QuickDeploy", shell_output("#{bin}/quickdeploy --help")
+    end
+  end
