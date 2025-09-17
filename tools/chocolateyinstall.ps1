d9d3cb39ce682143b03d439508bd55bf19bf9973
Maximilian Kürschner <mail@programonaut.com>
2025-09-17 21:51:21 +0200

    remove .git from package

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = 'Stop'
 $packageName = 'quickdeploy'
 $toolsDir   = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
-$version    = '0.3.11'
+$version    = '0.3.4'
 # $osArch     = if ([Environment]::Is64BitOperatingSystem) { 'amd64' } else { 'x86' }
 $osArch     = 'amd64'
 
@@ -15,4 +15,4 @@ Get-ChocolateyWebFile `
     -FileFullPath $exePath `
     -Url $binaryUrl `
     -ChecksumType 'sha256' `
-    -Checksum 'sha256:bb98a5f6a9e9e5062ff80baa6d67c2aca5bdde3d85ee08dfbac4a9dfaf30aa03'
+    -Checksum 'sha256:236131b82840393628b1a5db79f0f8304eeb8573d8ee23ad507a447aef78f9bf'
