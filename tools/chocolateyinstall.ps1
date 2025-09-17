dca44c685f2a0b10749e2ad9a5ecf120835468ae
Maximilian Kürschner <mail@programonaut.com>
2025-09-17 15:22:44 +0200

    restore osArch

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsDir   = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $version    = '0.3.4'
 # $osArch     = if ([Environment]::Is64BitOperatingSystem) { 'amd64' } else { 'x86' }
-$osArch     = '0.3.4'
+$osArch     = 'amd64'
 
 $binaryUrl  = "https://quickdeploy.dev/api/download/windows-$osArch"
 $exePath    = Join-Path $toolsDir 'quickdeploy.exe'
